2ef096f71bf06e51d1e1640ba12fec0a31bddb58
abrikos <abrikos1100@gmail.com>
2021-10-13 16:23:40 +0300

    added time measurement and control sums

--- main.cpp
+++ main.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
-#include <chrono>
 #include <string>
 
 #include "matrices.cpp"
 #include "tests.cpp"
-
-
-double time() {
-    return std::chrono::duration_cast<std::chrono::nanoseconds>(
-        std::chrono::high_resolution_clock::now().time_since_epoch()).count()
-        / 1e9;
-}
+#include "timetest.cpp"
 
 
 int main(int argc, char **args) {
@@ -38,4 +31,21 @@ int main(int argc, char **args) {
     test_dot_product(N, seed, N <= 10);
     test_mat_vec_product(N, seed, cs_seed, N <= 10);
     test_linear_combination(N, seed, cs_seed, N <= 10);
+
+    std::vector<double> csum;
+    int rep = 100;
+    int n = 1000000;
+    std::cout << "avg time of mat-vec product, n=" << n << ", repeats=" << rep
+        << ": " << avg_time_of_mat_vec_product(n, rep, seed, cs_seed, csum) << std::endl;
+    PRINT_VECTOR(csum, "control sum");
+
+    n = 10000000;
+    std::cout << "avg time of mat-vec product, n=" << n << ", repeats=" << rep
+        << ": " << avg_time_of_linear_combination(n, rep, seed, cs_seed, csum) << std::endl;
+    PRINT_VECTOR(csum, "control sum");
+
+    double dot = -1;
+    std::cout << "avg time of dot product, n=" << n << ", repeats=" << rep
+        << ": " << avg_time_of_dot_product(n, rep, seed, dot) << std::endl
+        << "dot : " << dot << std::endl;
 }
---
+++ timetest.cpp
@@ -0,0 +1,78 @@
+#ifndef TIMETEST_CPP
+#define TIMETEST_CPP
+
+#include <vector>
+#include <chrono>
+
+#include "matrices.cpp"
+#include "tests.cpp"
+
+
+double time() {
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(
+        std::chrono::high_resolution_clock::now().time_since_epoch()).count()
+        / 1e9;
+}
+
+
+double avg_time_of_mat_vec_product(size_t N, int repeats,
+        uint_fast64_t seed, uint_fast64_t cs_seed,
+        std::vector<double> &csum) {
+
+    std::vector<double> v, pr;
+    CSR_matrix a;
+
+    generate_vector(N, seed, v);
+    generate_matrix(N, seed, a);
+
+    double st = time();
+    for (int i = 0; i < repeats; ++i) {
+        product(a, v, pr);
+    }
+    st = time() - st;
+
+    csum = control_sum(pr, cs_seed);
+
+    return st / repeats;
+}
+
+
+double avg_time_of_linear_combination(size_t N, int repeats,
+        uint_fast64_t seed, uint_fast64_t cs_seed,
+        std::vector<double> &csum) {
+
+    // control sums are different with different repeats
+    std::vector<double> x, y;
+    generate_vector(N, seed, x);
+    generate_vector(N, seed, y);
+
+    double st = time();
+    for (int i = 0; i < repeats; ++i) {
+        linear_combination(0.72, 0.27, x, y); // this is cumulative sum for my laziness
+        // sum of coeffs less than 1 to avoid exponential growth
+    }
+    st = time() - st;
+
+    csum = control_sum(x, cs_seed);
+
+    return st / repeats;
+}
+
+
+double avg_time_of_dot_product(size_t N, int repeats,
+        uint_fast64_t seed, double &dot) {
+
+    std::vector<double> x, y;
+    generate_vector(N, seed, x);
+    generate_vector(N, seed, y);
+
+    double st = time();
+    for (int i = 0; i < repeats; ++i) {
+        dot = dot_product(x, y);
+    }
+    st = time() - st;
+
+    return st / repeats;
+}
+
+#endif
